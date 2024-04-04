a01f78772c1e92d75fb079a7a5c359b9ea91e680
Ahmed Allam <lamahmd122@gmail.com>
2024-04-04 08:09:45 +0200

    Basic Linear Search

---
+++ Searching Algorithm/Linear search/BasicLinearSearch.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * Performs linear search on an array to find the target element.
+ *
+ * Time Complexity: O(n), where n is the number of elements in the array.
+ *                  Linear search checks each element in the array sequentially until the target element is found or the end of the array is reached.
+ *
+ * @param array        : The array to search in.
+ * @param size         : The size of the array.
+ * @param checkElement : The element to search for within the array.
+ * @return             : The index of the found element if present, otherwise -1.
+ */
+int linearSearch(int array[], int size, int checkElement);
+
+int main()
+{
+
+  return 0;
+}
+
+int linearSearch(int array[], int size, int checkElement)
+{
+  int l_index = 0;
+
+  for (l_index = 0; l_index < size; l_index++)
+  {
+    if (array[l_index] == checkElement)
+    {
+      return l_index; // return the index of the element in the array
+    }
+  }
+
+  return -1;
+}
---
+++ Searching Algorithm/Linear search/SequentialSearch.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * Performs sequential search on an array to find the target element.
+ *
+ * Time Complexity: O(n/2), where n is the number of elements in the array.
+ *                  Sequential search checks each element in the array sequentially from both ends towards the middle until the target element is found or the search ends.
+ *
+ * @param array        : The array to search in.
+ * @param size         : The size of the array.
+ * @param checkElement : The element to search for within the array.
+ * @return             : The index of the found element if present, otherwise -1.
+ */
+int sequentialSearch(int array[], int size, int checkElement);
+
+int main()
+{
+
+  return 0;
+}
+
+int sequentialSearch(int array[], int size, int checkElement)
+{
+  int l_leftIndex = 0;
+  int l_rightIndex = size - l_leftIndex - 1;
+
+  while(l_leftIndex <= l_rightIndex)
+  {
+    if (array[l_leftIndex] == checkElement)
+    {
+      return  l_leftIndex; 
+    }
+    else if (array[l_rightIndex] == checkElement)
+    {
+      return l_rightIndex;
+    }
+    l_leftIndex++;
+    l_rightIndex--;
+  }
+
+  return -1;
+}
