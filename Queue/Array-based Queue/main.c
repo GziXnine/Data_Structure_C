9972b7cc97cf3f7e95329deb835b9bad01f9981e
Ahmed Allam <lamahmd122@gmail.com>
2024-04-07 12:30:16 +0200

    main

---
+++ Queue/Array-based Queue/main.c
@@ -0,0 +1,12 @@
+#include "queue_ds.h"
+
+queue_ds_t *queuePtr = NULL;
+void *itemPtr = NULL;
+queue_status_t retVal = QUEUE_NOK;
+uint32_t queueSize = ZERO_INIT, queueCount = ZERO_INIT;
+
+int main()
+{
+
+  return 0;
+}
