a424640648f13a0668a6a7f31015aeecf2e73135
Ahmed Allam <lamahmd122@gmail.com>
2024-04-08 21:26:43 +0200

    main.c

---
+++ Linked List/Singly Linked List/main.c
@@ -0,0 +1,12 @@
+#include "linkedList_ds.h"
+
+node_ds_t *ListHead = NULL;
+linkedList_status_t retVal = LINKED_LIST_NOK;
+uint32_t UserChoice = ZERO_INIT, item = ZERO_INIT;
+uint32_t position = ZERO_INIT;
+
+int main()
+{
+
+  return 0;
+}
