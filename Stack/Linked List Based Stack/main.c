23e56a27db544edbbb3766669910ad627f415bea
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2024-04-19 17:29:55 +0200

    main.c

---
+++ Stack/Linked List Based Stack/main.c
@@ -0,0 +1,12 @@
+#include "stack_ds.h"
+
+stack myStack;
+return_status_t retVal = STACK_NOK;
+uint32_t value = ZERO_INIT, poppedValue = ZERO_INIT, toppedValue = ZERO_INIT;
+sint32_t stackSize = ZERO_INIT;
+
+int main()
+{
+
+  return 0;
+}
