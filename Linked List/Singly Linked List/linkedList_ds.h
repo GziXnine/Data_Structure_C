ea94939ba4e7ead1503ee3f8e4955ceb940113c9
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2024-04-20 02:15:11 +0200

    linkedList_ds.h

--- Linked List/Singly Linked List/linkedList_ds.h
+++ Linked List/Singly Linked List/linkedList_ds.h
@@ -3,18 +3,27 @@
 
 #include "std_types.h" // Include necessary header file for standard data types
 
-typedef struct Node
-{
-  uint32_t NodeData;     // Data stored in the node
-  struct Node *NodeLink; // Pointer to the next node in the linked list
+#define LINKEDLIST_DEBUG // Enable debug mode for printing additional information
 
-} node_ds_t; // Define node data structure for linked list
+typedef struct listNode
+{
+  struct listNode *next;
+  uint32_t data;
+
+} listNode_t; // Define node data structure for linked list
+
+typedef struct list
+{
+  listNode_t *head; // Point to the head of the linked
+  uint32_t size;    // Number of elements in the linked list
+
+} node_ds_t;
 
 typedef enum
 {
   LINKED_LIST_OK,           // Linked list operation performed successfully
   LINKED_LIST_NOK,          // Linked list operation not performed successfully
-  INVALID_POSOTION,         // Linked list not full
+  INVALID_POSOTION,         // Invalid position provided for the linked list operation
   LINKED_LIST_EMPTY,        // Linked list is empty
   LINKED_LIST_NULL_POINTER, // NULL pointer passed to the linked list operations
   LINKED_LIST_ALLOC_FAIL,   // Memory allocation for linked list failed
@@ -23,106 +32,154 @@ typedef enum
 } linkedList_status_t; // Define return status enumeration for linked list operations
 
 /**
- * @brief  Insert a node at the beginning of the linked list
+ * @brief  Creates an empty linked list.
+ *
+ * This function creates an empty linked list by initializing the head pointer to NULL.
+ *
+ * @param  List : Pointer to the structure representing the linked list.
+ *
+ * @retval Status indicating success or failure.
+ */
+linkedList_status_t createList(node_ds_t *List);
+
+/**
+ * @brief  Inserts a node at the beginning of the linked list.
  *
  * This function inserts a new node at the beginning of the linked list.
  * It updates the head pointer to point to the newly inserted node.
  *
- * @param  List : Pointer to the pointer of the head of the linked list
- * @param  itemPtr : Pointer to the data to be stored in the new node
- * @param  retVal : Pointer to a variable where the status of the operation will be stored
+ * @param  List : Pointer to the structure representing the linked list.
+ * @param  itemPtr : data to be stored in the new node.
  *
- * @retval Pointer to the newly inserted node
+ * @retval Status indicating success or failure.
  */
-node_ds_t *Insert_Node_At_Beginning(node_ds_t **List, uint32_t *itemPtr, linkedList_status_t *retVal);
+linkedList_status_t Insert_Node_At_Beginning(node_ds_t *List, uint32_t itemPtr);
 
 /**
- * @brief  Insert a node at the end of the linked list
+ * @brief  Inserts a node at the end of the linked list.
  *
  * This function inserts a new node at the end of the linked list.
  * It traverses the list to find the last node and appends the new node to it.
  *
- * @param  List : Pointer to the pointer of the head of the linked list
- * @param  itemPtr : Pointer to the data to be stored in the new node
+ * @param  List : Pointer to the structure representing the linked list.
+ * @param  itemPtr : data to be stored in the new node.
  *
- * @retval Status indicating success or failure
+ * @retval Status indicating success or failure.
  */
-linkedList_status_t Insert_Node_At_End(node_ds_t **List, uint32_t *itemPtr);
+linkedList_status_t Insert_Node_At_End(node_ds_t *List, uint32_t itemPtr);
 
 /**
- * @brief  Insert a node after a specified node in the linked list
+ * @brief  Inserts a node after a specified node in the linked list.
  *
  * This function inserts a new node after a specified node in the linked list.
  * It searches for the specified node and inserts the new node after it.
  *
- * @param  List : Pointer to the head of the linked list
- * @param  itemPtr : Pointer to the data to be stored in the new node
- * @param  Position : Pointer to the position after which the new node will be inserted
+ * @param  List : Pointer to the structure representing the linked list.
+ * @param  itemPtr : data to be stored in the new node.
+ * @param  Position : position after which the new node will be inserted.
  *
- * @retval Status indicating success or failure
+ * @retval Status indicating success or failure.
  */
-linkedList_status_t Insert_Node_At_Position(node_ds_t *List, uint32_t *itemPtr, uint32_t *Position);
+linkedList_status_t Insert_Node_At_Position(node_ds_t *List, uint32_t itemPtr, uint32_t Postion);
 
 /**
- * @brief  Delete a node from the beginning of the linked list
+ * @brief  Deletes the node at the beginning of the linked list.
  *
- * This function deletes the first node from the linked list.
+ * This function deletes the node at the beginning of the linked list.
  * It updates the head pointer to point to the next node.
  *
- * @param  List : Pointer to the pointer of the head of the linked list
+ * @param  List : Pointer to the structure representing the linked list.
+ * @param  itemPtr : Pointer to store the data of the deleted node.
  *
- * @retval Status indicating success or failure
+ * @retval Status indicating success or failure.
  */
-linkedList_status_t Delete_Node_At_Beginning(node_ds_t **List);
+linkedList_status_t Delete_Node_At_Beginning(node_ds_t *List, uint32_t *itemPtr);
 
 /**
- * @brief  Delete a specified node from the linked list
+ * @brief  Deletes the node at the specified position in the linked list.
  *
- * This function deletes a specified node from the linked list.
- * It searches for the specified node and removes it from the list.
+ * This function deletes the node at the specified position in the linked list.
+ * It searches for the specified position and removes the corresponding node.
  *
- * @param  List : Pointer to the head of the linked list
- * @param  itemPtr : Pointer to the data to be deleted
+ * @param  List : Pointer to the structure representing the linked list.
+ * @param  Position : Position of the node to be deleted.
+ * @param  itemPtr : Pointer to store the data of the deleted node.
  *
- * @retval Status indicating success or failure
+ * @retval Status indicating success or failure.
  */
-linkedList_status_t Delete_Node_At_Position(node_ds_t *List, uint32_t *itemPtr);
+linkedList_status_t Delete_Node_At_Position(node_ds_t *List, uint32_t Position, uint32_t *itemPtr);
 
 /**
- * @brief  Delete a node from the end of the linked list
+ * @brief  Deletes the node at the end of the linked list.
  *
  * This function deletes the last node from the linked list.
  * It updates the link of the second-to-last node to NULL.
  *
- * @param  List : Pointer to the head of the linked list
+ * @param  List : Pointer to the structure representing the linked list.
+ * @param  itemPtr : Pointer to store the data of the deleted node.
  *
- * @retval Status indicating success or failure
+ * @retval Status indicating success or failure.
  */
-linkedList_status_t Delete_Node_At_End(node_ds_t *List);
+linkedList_status_t Delete_Node_At_End(node_ds_t *List, uint32_t *itemPtr);
 
 /**
- * @brief  Display all nodes in the linked list
+ * @brief  Displays the data of all nodes in the linked list.
  *
- * This function displays the data of all nodes in the linked list.
- * It traverses the list and prints the data of each node.
+ * This function traverses the linked list and prints the data of each node.
  *
- * @param  List : Pointer to the head of the linked list
+ * @param  List : Pointer to the structure representing the linked list.
  *
- * @retval Status indicating success or failure
+ * @retval Status indicating success or failure.
  */
 linkedList_status_t Display_All_Nodes(node_ds_t *List);
 
 /**
- * @brief  Get the length (number of nodes) of the linked list
+ * @brief  Calculates the length (number of nodes) of the linked list.
  *
- * This function calculates the length of the linked list.
- * It traverses the list and counts the number of nodes.
+ * This function traverses the linked list and counts the number of nodes.
  *
- * @param  List : Pointer to the head of the linked list
- * @param  retVal : Pointer to a variable where the status of the operation will be stored
+ * @param  List : Pointer to the structure representing the linked list.
+ * @param  size : Pointer to a variable where the length of the linked list will be stored.
  *
- * @retval Number of nodes in the linked list
+ * @retval Status indicating success or failure.
  */
-uint32_t Get_Length(node_ds_t *List, linkedList_status_t *retVal);
+linkedList_status_t Get_Length(node_ds_t *List, uint32_t *size);
+
+/**
+ * @brief  Destroys the linked list and frees the allocated memory.
+ *
+ * This function destroys the linked list by freeing the memory allocated for each node.
+ *
+ * @param  List : Pointer to the structure representing the linked list.
+ *
+ * @retval Status indicating success or failure.
+ */
+linkedList_status_t destroyList(node_ds_t *List);
+
+/**
+ * @brief  Retrieves the data at a specified position in the linked list.
+ *
+ * This function retrieves the data stored at the specified position in the linked list.
+ *
+ * @param  List : Pointer to the structure representing the linked list.
+ * @param  Position : Position of the node whose data is to be retrieved.
+ * @param  itemPtr : Pointer to store the retrieved data.
+ *
+ * @retval Status indicating success or failure.
+ */
+linkedList_status_t retrieveList(node_ds_t *List, uint32_t Position, uint32_t *itemPtr);
+
+/**
+ * @brief  Replaces the data at a specified position in the linked list.
+ *
+ * This function replaces the data stored at the specified position in the linked list with a new value.
+ *
+ * @param  List : Pointer to the structure representing the linked list.
+ * @param  Position : Position of the node whose data is to be replaced.
+ * @param  itemPtr : New data value to be stored at the specified position.
+ *
+ * @retval Status indicating success or failure.
+ */
+linkedList_status_t replaceList(node_ds_t *List, uint32_t Position, uint32_t itemPtr);
 
 #endif // ! _LINKEDLIST_DS_H_
