ea64bcc733c790d265d7a4e2613de234f0637521
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2024-04-20 22:20:03 +0200

    main.c

---
+++ Linked List/Doubly Linked List/main.c
@@ -0,0 +1,11 @@
+#include "LinkedList_ds.h"
+
+node_ds_t ListHead;
+linkedList_status_t retVal = LINKED_LIST_NOK;
+uint32_t item = ZERO_INIT, size = ZERO_INIT, position = ZERO_INIT;
+
+int main()
+{
+  
+  return 0;
+}
