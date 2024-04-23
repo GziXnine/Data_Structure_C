c1aba0add1b8fdf946ca6a5f6a8d3c2426178251
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2024-04-23 21:09:39 +0200

    main.c

---
+++ Tree/Binary Search Tree/main.c
@@ -0,0 +1,20 @@
+#include "binarySearchTree.h"
+
+tree myTree;
+uint32_t size = ZERO_INIT, depth = ZERO_INIT;
+uint32_t checkElement = ZERO_INIT, deletedValue = ZERO_INIT;
+return_status_t retVal = BINARY_TREE_NOK; 
+
+// Function prototype
+void printNode(uint32_t data);
+
+int main()
+{
+ 
+  return 0;
+}
+
+void printNode(uint32_t data)
+{
+  printf("%u ", data);
+}
