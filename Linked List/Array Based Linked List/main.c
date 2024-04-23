a11e04a223a4143c47e3e4bf1301d6e8b819d2a0
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2024-04-24 01:23:02 +0200

    main.c

---
+++ Linked List/Array Based Linked List/main.c
@@ -0,0 +1,11 @@
+#include "linkedList.h"
+
+listNode_t List;
+uint32_t length = ZERO_INIT, position = ZERO_INIT, data = ZERO_INIT;
+linkedList_status_t retVal = LINKED_LIST_NOK;
+
+int main()
+{
+
+  return 0;
+}
