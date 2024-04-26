20ea0e411868b021396e6de38b9f4d931a3aafca
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2024-04-26 07:29:40 +0300

    main.c

--- Graph/Undirected Graph/main.c
+++ Graph/Undirected Graph/main.c
@@ -1,8 +1,8 @@
 #include "graph_ds.h"
 
-graph_ds_t graph;
-uint32_t numVertices = ZERO_INIT, numEdges = ZERO_INIT;
-uint32_t degree = ZERO_INIT, numAdjacent = ZERO_INIT;
+Graph graph;
+uint32_t numVerticesResult = ZERO_INIT, numEdgesResult = ZERO_INIT;
+uint32_t numVertices = ZERO_INIT, defaultData = ZERO_INIT;
 return_status_t status = GRAPH_NOK;
 
 int main()
