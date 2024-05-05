f2227bace72791b52acf046596d64508f1ebef21
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2024-05-05 05:33:22 +0300

    main.c

---
+++ Array/Dynamic 1D_Array (vector)/main.c
@@ -0,0 +1,12 @@
+#include "array_1d.h"
+
+Vector vec;
+return_status_t status = VECTOR_NOK;
+uint32_t capacity = ZERO_INIT, data = ZERO_INIT, value = ZERO_INIT;
+uint16_t size = ZERO_INIT;
+
+int main()
+{
+
+  return 0;
+}
