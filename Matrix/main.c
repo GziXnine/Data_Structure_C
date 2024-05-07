f5e53b1302d2b7f78503cfe705da1e6dc04a55a1
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2024-05-07 23:37:02 +0300

    main.c

---
+++ Matrix/main.c
@@ -0,0 +1,12 @@
+#include "array_2d.h"
+
+Matrix matrix;
+uint32_t value = ZERO_INIT;
+uint16_t rows = ZERO_INIT, cols = ZERO_INIT;
+return_status_t status = MATRIX_NOK;
+
+int main()
+{
+
+  return 0;
+}
