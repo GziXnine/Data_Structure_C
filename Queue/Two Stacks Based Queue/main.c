e2b1018af22dfc3d444505103490492e4113d58f
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2024-05-08 12:48:06 +0300

    main.c

---
+++ Queue/Two Stacks Based Queue/main.c
@@ -0,0 +1,12 @@
+#include "queue_ds.h"
+
+queue_ds_t queue;
+uint32_t queueSize = ZERO_INIT, queueCount = ZERO_INIT;
+uint32_t itemPtr = ZERO_INIT;
+queue_status_t retVal = QUEUE_NOK;
+
+int main()
+{
+
+  return 0;
+}
