92b62a5e3e6cc6faecde7603457eb5e06e9313f8
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2024-05-09 20:45:22 +0300

    main.c

---
+++ Set/Array Based Set/main.c
@@ -0,0 +1,11 @@
+#include "set_ds.h"
+
+return_status_t retVal = SET_NOK;
+ArraySet *set1, *set2, *unionset, *intersect, *difference, *complement;
+uint32_t elementsToAdd[] = {0}, elementsToRemove[] = {0};
+
+int main() 
+{
+
+  return 0;
+}
